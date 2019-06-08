364795fa871c4b81ae70505e7358ed1ec61159ed
alanis30017084 <alanis.ck@gmail.com>
2019-06-08 15:29:01 +1200

    Completed functionality

--- methods_login_system/methods_login_system/Program.cs
+++ methods_login_system/methods_login_system/Program.cs
@@ -10,24 +10,43 @@ namespace methods_login_system
     {
         static void Main(string[] args)
         {
-            Console.Write("<<< Create new login >>>\n\nPlease enter a username at least 8 characters long: ");
-            string username = Console.ReadLine();
+            Console.Write("<<< Create new login >>>\n");
             int checkuser = 2;
+            int checkpass = 2;
+            
+            do
+            {
+                Console.Write("\nPlease enter a username at least 8 characters long: ");
+                string username = Console.ReadLine();
 
-            if (ValidateUsername(username, checkuser) == 1)
-            {
-                Console.Write("You have entered a valid username.\nPlease enter a password at least 8 characters long: ");
-                string password = Console.ReadLine();
+                if (ValidateUsername(username, checkuser) == 1)
+                {
+                    Console.Write("You have entered a valid username.");
+                    do {
+                    Console.Write("\n\nPlease enter a password at least 8 characters long: ");
+                    string pass = Console.ReadLine();
+                    Console.Write("Please enter this password again: ");
+                    string pass2 = Console.ReadLine();
+
+                    if (ValidatePass(pass, pass2, checkpass) == 1)
+                    {
+                        Console.WriteLine($"\nUsername and Password have been accepted and set.\n\nYour details are as follows...\nUsername: {username}\nPassword: {pass}");
+                            checkuser = 1;
+                            checkpass = 1;
+                        }
+                    else if (ValidatePass(pass, pass2, checkpass) == 0)
+                    {
+                        Console.WriteLine("\nInvalid password.\nPlease check it is more than 8 characters and both entries match.");
+                    }
+                    } while (checkpass != 1);
+                }
+
+                else if (ValidateUsername(username, checkuser) == 0)
+                {
+                    Console.WriteLine("Your username is invalid. Please check it is more than 8 characters.");
+                }
                 
-            }
-            if (ValidateUsername(username, checkuser) == 0)
-            {
-                Console.WriteLine("This username is less than 8 characters. Please try again.");
-            }
-            else
-            {
-                Console.WriteLine("Sorry, that input is invalid. Please try again.");
-            }
+            } while (checkuser != 1);
             Console.ReadLine();
         }
 
@@ -44,6 +63,18 @@ namespace methods_login_system
             return checkuser;
         }
 
-        
+        static int ValidatePass(string pass, string pass2, int checkpass)
+        {
+            if (pass == pass2 && pass.Length >= 8)
+            {
+                checkpass = 1;
+            }
+            else
+            {
+                checkpass = 0;
+            }
+            return checkpass;
+        }
+                
     }
 }
